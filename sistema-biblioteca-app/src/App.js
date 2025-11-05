a8cb5c4ac849a0581c02730fd8798a5547c9aba1
Giovanna Pereira <giovannapper@gmail.com>
2025-11-05 10:44:17 -0300

    Configuranco o App.js e a navbar.js

--- sistema-biblioteca-app/src/App.js
+++ sistema-biblioteca-app/src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import 'bootswatch/dist/flatly/bootstrap.css';
 import 'toastr/build/toastr.min.css';
-import 'toastr/build/toastr.css';
 import Navbar from './components/navbar';
 import Rotas from './rotas';
 
 class App extends React.Component {
   render() {
     return (
-      <>
+      <div className='container'>
         <Navbar />
-        <div className="container mt-4">
-          <Rotas />
-        </div>
-      </>
+        <Rotas />
+      </div>
     );
   }
 }
--- sistema-biblioteca-app/src/components/navbar.js
+++ sistema-biblioteca-app/src/components/navbar.js
@@ -1,49 +1,82 @@
-import React from 'react';
-import 'bootswatch/dist/flatly/bootstrap.css';
+import React from "react";
+import "bootswatch/dist/flatly/bootstrap.css";
 
-import NavbarItem from './navbarItem';
+import NavbarItem from "./navbarItem";
 
 function Navbar(props) {
   return (
-    <div className='navbar navbar-expand-lg fixed-top navbar-dark bg-primary'>
-      <div className='container'>
-        <a href='/' className='navbar-brand'>
-          SCAC
+    <div className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          Sistema de Biblioteca
         </a>
         <button
-          className='navbar-toggler'
-          type='button'
-          data-toggle='collapse'
-          data-target='#navbarResponsive'
-          aria-controls='navbarResponsive'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          <span className='navbar-toggler-icon'></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div className='collapse navbar-collapse' id='navbarResponsive'>
-          <ul className='navbar-nav'>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav">
             <NavbarItem
-              render='true'
-              href='/listagem-clientes'
-              label='Clientes'
+              render="true"
+              href="/listagem-clientes"
+              label="Clientes"
             />
           </ul>
-          <ul className='navbar-nav'>
-            <NavbarItem render='true' href='/listagem-exemplares' label='Exemplares' />
-          </ul>
-          <ul className='navbar-nav'>
+          <ul className="navbar-nav">
             <NavbarItem
-              render='true'
-              href='/listagem-multas'
-              label='Multas'
+              render="true"
+              href="/listagem-exemplares"
+              label="Exemplares"
             />
           </ul>
-          <ul className='navbar-nav'>
-            <NavbarItem render='true' href='/login' label='Entrar' />
+          <ul className="navbar-nav">
+            <NavbarItem 
+             render="true" 
+             href="/listagem-multas" 
+             label="Multas" />
           </ul>
-          <ul className='navbar-nav'>
-            <NavbarItem render='true' href='/' label='Sair' />
+          <ul className="navbar-nav">
+            <NavbarItem 
+             render="true" 
+             href="/listagem-autores" 
+             label="Autores" />
+          </ul>
+          <ul className="navbar-nav">
+            <NavbarItem 
+             render="true" 
+             href="/listagem-idiomas" 
+             label="Idiomas" />
+          </ul>
+          <ul className="navbar-nav">
+            <NavbarItem 
+             render="true" 
+             href="/listagem-secoes" 
+             label="Seções" />
+          </ul>
+          <ul className="navbar-nav">
+            <NavbarItem 
+             render="true" 
+             href="/listagem-editoras" 
+             label="Editoras" />
+          </ul>
+          <ul className="navbar-nav">
+            <NavbarItem 
+             render="true" 
+             href="/listagem-generos" 
+             label="Gêneros" />
+          </ul>
+          <ul className="navbar-nav">
+            <NavbarItem 
+             render="true" 
+             href="/listagem-obras" 
+             label="Obras" />
           </ul>
         </div>
       </div>
