6ca69dd6c2e19ec3f57dd225a6b4e6d290ef5d14
M-Tereza <mtereza1999@gmail.com>
2025-12-17 20:11:25 -0300

    Adição de novas classes

--- sistema-biblioteca-app/src/config/axios.js
+++ sistema-biblioteca-app/src/config/axios.js
@@ -4,14 +4,18 @@ export const API_URLS = {
   clientes: "https://my-json-server.typicode.com/GiovPereira/json-clientes",
   multas: "https://my-json-server.typicode.com/GiovPereira/json-clientes",
   exemplares: "https://my-json-server.typicode.com/GiovPereira/json-clientes",
-  status: "https://my-json-server.typicode.com/GiovPereira/json-clientes",
+  statusEmprestimos: "https://my-json-server.typicode.com/GiovPereira/json-clientes",
   obras: "https://my-json-server.typicode.com/GiovPereira/json-obras",
   generos: "https://my-json-server.typicode.com/GiovPereira/json-obras",
   editoras: "https://my-json-server.typicode.com/GiovPereira/json-obras",
+  statusReservas: "https://my-json-server.typicode.com/GiovPereira/json-obras",
   idiomas: "https://my-json-server.typicode.com/GiovPereira/json-geral",
   secoes: "https://my-json-server.typicode.com/GiovPereira/json-geral",
   autores: "https://my-json-server.typicode.com/GiovPereira/json-geral",
   emprestimos: "https://my-json-server.typicode.com/GiovPereira/json-emprestimos",
+  reservas: "https://my-json-server.typicode.com/GiovPereira/json-emprestimos",
+  duracaoPadraoEmprestimos: "https://my-json-server.typicode.com/GiovPereira/json-emprestimos",
+  valorDiarioMultas: "https://my-json-server.typicode.com/GiovPereira/json-emprestimos"
 };
 
 export default axios;
